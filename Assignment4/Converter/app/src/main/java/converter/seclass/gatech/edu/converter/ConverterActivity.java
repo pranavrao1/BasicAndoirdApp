f1a1cb4133718e4182f951a33cdd613019f6baed
pranavrao1 <pranavrao1@gmail.com>
2016-02-17 01:49:12 -0800

    Creating Application

---
+++ Assignment4/Converter/app/src/main/java/converter/seclass/gatech/edu/converter/ConverterActivity.java
@@ -0,0 +1,13 @@
+package converter.seclass.gatech.edu.converter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.converter_activity);
+    }
+}
